2fd2226e2b979660b0ca65319dd998d26be122d9
Christian Häggström <christian.haggstrom@orexplore.com>
2018-03-27 22:25:42 +0200

    Do checksum correctly, now it works fine.

--- Makefile
+++ Makefile
@@ -5,12 +5,12 @@ DEVICE=eth0
 build: bpf.o
 
 bpf.o: bpf.c
-	clang -O2 -target bpf -I/usr/include/x86_64-linux-gnu -c $< -o $@
+	clang -Wall -O2 -target bpf -I/usr/include/x86_64-linux-gnu -c $< -o $@
 
 run: bpf.o
-	sudo tc qdisc add dev $(DEVICE) clsact
-	sudo tc filter add dev $(DEVICE) ingress obj bpf.o verbose
+	sudo tc qdisc add dev $(DEVICE) clsact || true
+	sudo tc filter add dev $(DEVICE) ingress bpf obj bpf.o verbose
 delete:
-	sudo tc filter delete dev $(DEVICE) parent ffff:
+	sudo tc filter delete dev $(DEVICE) ingress
 show:
-	sudo tc filter show dev ingress $(DEVICE)
+	sudo tc filter show dev $(DEVICE) ingress
--- bpf.c
+++ bpf.c
@@ -8,6 +8,7 @@
 #include "bpf_helpers.h"
 
 #define offsetof __builtin_offsetof
+#define htons(x) __constant_htons(x)
 
 #define IP_CSUM_OFF (ETH_HLEN + offsetof(struct iphdr, check))
 
@@ -17,11 +18,23 @@ int fixup_ip_checksum(struct __sk_buff *skb)
     if (load_half(skb, offsetof(struct ethhdr, h_proto)) != ETH_P_IP)
         return 0;
 
-    __u16 checksum = 0;
-    for (int i = 0; i < sizeof(struct iphdr); i += 2) {
-        checksum += ~load_half(skb, ETH_HLEN + i);
-    }
-    bpf_l3_csum_replace(skb, IP_CSUM_OFF, 0xffff, checksum, 2);
+    // Calculate one's complement sum of IP header except checksum word at
+    // offset 24.
+    __u64 checksum = 0;
+    checksum += load_half(skb, 14);
+    checksum += load_word(skb, 16);
+    checksum += load_word(skb, 20);
+    checksum += load_word(skb, 26);
+    checksum += load_word(skb, 30);
+
+    checksum = (checksum & 0xffff) + (checksum >> 16);
+    checksum = (checksum & 0xffff) + (checksum >> 16);
+    checksum = (checksum & 0xffff) + (checksum >> 16);
+
+    // Write back correct checksum in the packet.
+    __u16 checksum16 = htons(~checksum);
+
+    bpf_skb_store_bytes(skb, IP_CSUM_OFF, &checksum16, 2, 0);
 
     return 0;
 }
